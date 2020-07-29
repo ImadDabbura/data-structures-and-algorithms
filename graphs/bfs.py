2e16fc6658a83fb15fda06b5971de49fd8be4f69
Imad <imad.dabbura@hotmail.com>
2020-07-29 11:00:51 -0500

    Add BSF_complete method to BFS

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -9,9 +9,25 @@ def BFS(g, u, discovered, component=0, outgoing=True):
     while len(level) > 0:
         next_level = []
         for u in level:
-            for e in g.incident_edges(u):
+            for e in g.incident_edges(u, outgoing):
                 v = e.opposite(u)
                 if v not in discovered:
                     discovered[v] = (e, component)
                     next_level.append(v)
         level = next_level
+
+
+def BFS_complete(g, outgoing=True):
+    """
+    Perform BFS for the entire graph. In the case where the graph is not
+    connected, we can count the number of vertces in the forest dictionary 
+    that has None value.
+    """
+    forest = {}
+    component = -1
+    for v in g.vertices():
+        if v not in forest:
+            component += 1
+            forest[v] = (None, component)
+            BFS(g, v, forest, component, outgoing)
+    return forest
